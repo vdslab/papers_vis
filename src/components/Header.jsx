50f8b78b1f3da767f47796ca2fec13c574bf4828
mry-E <eikimry2007@gmail.com>
2022-07-07 10:34:48 +0900

    first

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+      <header>
+        <div className="hero is-small is-info">
+          <div className="hero-body">
+            <h1 className="title" style={{fontSize: "150%"}}>Paper Vis</h1>
+          </div>
+        </div>
+      </header>
+    );
+  };
+  export default Header;
