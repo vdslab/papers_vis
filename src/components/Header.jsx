91f777dfab5305929f930b62ae1a4c53c80f271d
kumastry <chma19063@g.nihon-u.ac.jp>
2022-07-11 22:55:17 +0900

    add ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,27 @@
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 const Header = () => {
     return (
       <header>
         <div className="hero is-small is-info">
           <div className="hero-body">
-            <h1 className="title" style={{fontSize: "150%"}}>Paper Vis</h1>
+
+              <h1 className='title' style= {{position:'relative', top : "20px"}}  >paper viz</h1>
+
+        
+            <Grid container>
+
+            <Grid item sm={11}  >
+            <Box display="flex" justifyContent="flex-end" sx = {{position:'relative', bottom : "30px"}} >
+              <HelpOutlineIcon />
+              </Box>
+          </Grid>
+        </Grid>
           </div>
+
+
+          
         </div>
       </header>
     );
