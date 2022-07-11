016e6530a71e1cbd246e87f83dfb4b41acd49a80
kumastry <chma19063@g.nihon-u.ac.jp>
2022-07-11 17:25:32 +0900

    add some ui

---
+++ App/pages/select.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Select = () => {
+    return (
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    );
+}
+
+export default Select;
