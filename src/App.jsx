0c002baf052203cdc5e9b07584e26d82bfb7022c
kumastry <chma19063@g.nihon-u.ac.jp>
2022-07-14 08:50:40 +0900

    add network page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,24 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 import "bulma/css/bulma.css";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
+import Network from "./pages/network";
 
 export default function App() {
     return (
         <div>
             <Header />
-            <Main />
+            <BrowserRouter>
+                <Routes>
+                    <Route path = "/" element = { <Main/> } />
+                    <Route path = "/network" element = {<Network/>} />
+                </Routes>
+            </BrowserRouter>
+            
             <Footer />
         </div>
     );
