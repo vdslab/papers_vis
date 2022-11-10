f7f0b5b2055a27c13e54b7112458bf787128de6d
kumastry <chma19063@g.nihon-u.ac.jp>
2022-11-11 00:20:14 +0900

    プログレスを別ファイルに移動

--- src/components/NetworkGraph.jsx
+++ src/components/NetworkGraph.jsx
@@ -6,10 +6,7 @@ import objectArray2ArrayByKey from "../objectArray2ArraybyKey";
 import { noData } from "pg-protocol/dist/messages";
 import LabelProgress from '../components/LabelProgress';
 import { useParams } from "react-router-dom";
-import CircularProgress from '@mui/material/CircularProgress';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import PropTypes from 'prop-types';
+import CircularProgressWithLabel from "./CircularProgressWithLabel";
 
 /*
 todo
@@ -69,38 +66,7 @@ const ZoomableSVG= ({ children, width, height }) => {
     }
 }
 
-function CircularProgressWithLabel(props) {
-    return (
-      <Box sx={{ position: 'relative', display: 'inline-flex' }}>
-        <CircularProgress variant="determinate" {...props} />
-        <Box
-          sx={{
-            top: 0,
-            left: 0,
-            bottom: 0,
-            right: 0,
-            position: 'absolute',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <Typography variant="caption" component="div" color="text.secondary">
-            {`${Math.round(props.value)}%`}
-          </Typography>
-        </Box>
-      </Box>
-    );
-  }
 
-  CircularProgressWithLabel.propTypes = {
-    /**
-     * The value of the progress indicator for the determinate variant.
-     * Value between 0 and 100.
-     * @default 0
-     */
-    value: PropTypes.number.isRequired,
-  };
   
 
 const NetworkGraph = ({detail, setDetail, nodeLabel, loading, setLoading, reloading}) => {
